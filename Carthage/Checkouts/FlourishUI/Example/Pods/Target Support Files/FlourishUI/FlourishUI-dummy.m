185cd835fae72b0badc2b3937bd6e7e686c0deac
Jose Luis Molina <joseluis@theagilemonkeys.com>
2017-02-24 12:39:18 +0000

    .gitignore is now working

--- Carthage/Checkouts/FlourishUI/Example/Pods/Target Support Files/FlourishUI/FlourishUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FlourishUI : NSObject
-@end
-@implementation PodsDummy_FlourishUI
-@end
--- Carthage/Checkouts/FlourishUI/Example/Pods/Target Support Files/Pods-FlourishUI_Example/Pods-FlourishUI_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FlourishUI_Example : NSObject
-@end
-@implementation PodsDummy_Pods_FlourishUI_Example
-@end
